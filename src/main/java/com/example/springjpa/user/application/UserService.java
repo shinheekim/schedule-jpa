2acb0de0589895e8f4f5eea368356141e18711f7
shinheekim <shinhee1241@gmail.com>
2024-10-16 01:39:05 +0900

    Style: userid로 user 객체 가져오는 중복코드 메서드로 빼기

--- src/main/java/com/example/springjpa/user/application/UserService.java
+++ src/main/java/com/example/springjpa/user/application/UserService.java
@@ -27,15 +27,18 @@ public class UserService {
     }
 
     public UserResponse findOneUser(Long id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("해당 유저가 존재하지 않습니다."));
+        User user = findUserById(id);
         return UserResponse.from(user);
     }
 
     public void update(Long id, UserUpdateRequest request) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("해당 유저가 존재하지 않습니다."));
+        User user = findUserById(id);
         user.update(request.name(), request.email());
         userRepository.save(user);
+    }
+
+    private User findUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("해당 유저가 존재하지 않습니다."));
     }
 }
