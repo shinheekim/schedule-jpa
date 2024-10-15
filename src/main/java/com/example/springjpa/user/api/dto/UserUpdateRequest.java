9567686bebf9e7592bf36485aa70e39b6a57698d
shinheekim <shinhee1241@gmail.com>
2024-10-16 01:49:30 +0900

    Feat: User 수정 request dto

---
+++ src/main/java/com/example/springjpa/user/api/dto/UserUpdateRequest.java
@@ -0,0 +1,12 @@
+package com.example.springjpa.user.api.dto;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record UserUpdateRequest(
+        @Size(max = 5)
+        String name,
+        @Pattern(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+        String email
+) {
+}
