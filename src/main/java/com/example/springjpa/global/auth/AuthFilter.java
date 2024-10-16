a3b41eb029e082e0f2d78c5e3db3564450a3bc5d
shinheekim <shinhee1241@gmail.com>
2024-10-17 00:33:22 +0900

    Style: 불필요한 주석 & 코드 삭제

--- src/main/java/com/example/springjpa/global/auth/AuthFilter.java
+++ src/main/java/com/example/springjpa/global/auth/AuthFilter.java
@@ -29,29 +29,23 @@ public class AuthFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        String url = httpServletRequest.getRequestURI();    // 아래 제어문으로 url 확인해야 함.
+        String url = httpServletRequest.getRequestURI();
 
         if (StringUtils.hasText(url) &&
-                (url.startsWith("/api/user") || url.startsWith("/css") || url.startsWith("/js"))
+                url.startsWith("/api/user")
         ) {
             log.info("인증 처리를 하지 않는 URL : " + url);
-            // 회원가입, 로그인 관련 API 는 인증 필요없이 요청 진행
-            chain.doFilter(request, response); // 다음 Filter 로 이동
+            chain.doFilter(request, response);
         } else {
-            // 나머지 API 요청은 인증 처리 진행
-            // 토큰 확인ㅡ @CookieValue 에노테이션 사용 불가해서 직접 해당 메서드 생성함. - DispatcherServlet보다 앞단이기 때문
             String tokenValue = tokenProvider.getTokenFromRequest(httpServletRequest);
 
-            if (StringUtils.hasText(tokenValue)) { // 토큰이 존재하면 검증 시작
-                // JWT 토큰 substring
+            if (StringUtils.hasText(tokenValue)) {
                 String token = tokenProvider.substringToken(tokenValue);
 
-                // 토큰 검증
                 if (!tokenProvider.validateToken(token)) {
                     throw new IllegalArgumentException("Token Error");
                 }
 
-                // 토큰에서 사용자 정보 가져오기
                 Claims info = tokenProvider.getUserInfoFromToken(token);
 
                 User user = userRepository.findByEmail(info.getSubject()).orElseThrow(() ->
@@ -59,7 +53,7 @@ public class AuthFilter implements Filter {
                 );
 
                 request.setAttribute("user", user);
-                chain.doFilter(request, response); // 다음 Filter 로 이동
+                chain.doFilter(request, response);
             } else {
                 throw new IllegalArgumentException("Not Found Token");
             }
--- src/main/java/com/example/springjpa/global/auth/LoggingFilter.java
+++ src/main/java/com/example/springjpa/global/auth/LoggingFilter.java
@@ -14,15 +14,12 @@ import java.io.IOException;
 public class LoggingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // 전처리
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;   // casting
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         String url = httpServletRequest.getRequestURI();
-        log.info(url);  // warn, error 등
+        log.info(url);
 
-        chain.doFilter(request, response); // 다음 Filter 로 이동
+        chain.doFilter(request, response);
 
-        // 후처리
         log.info("비즈니스 로직 완료");
-        // - 위에서 filter -> DispatcherServlet -> Controller에서 이것저것 수행 후 다시 DispatcherServlet이 응답을 보내면 일로 넘어옴!!
     }
 }
