9df6c9f4aef0351726b2a98605afdc675b19bd99
shinheekim <shinhee1241@gmail.com>
2024-10-17 15:43:16 +0900

    Style: schedule 예외 처리

--- src/main/java/com/example/springjpa/schedule/domain/Schedule.java
+++ src/main/java/com/example/springjpa/schedule/domain/Schedule.java
@@ -50,4 +50,8 @@ public class Schedule extends Timestamped {
             this.content = content;
         }
     }
+
+    public boolean isCreator(User user) {
+        return this.creator == user;
+    }
 }
